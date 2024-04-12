701580d13539fa6811ec69552c032f9314d8d626
Eros Rodriguez <163212935+SrPap@users.noreply.github.com>
2024-04-11 20:26:26 -0500

    Add files via upload

---
+++ egg_js/scripts/index.js
@@ -0,0 +1,31 @@
+// Actividad 1
+// Primer paso
+
+/* seleccionar una etiqueta del HTML */
+// const selector = document.getElementById("mensaje");
+
+/* escribir dentro de esa etiqueta con JS */
+// selector.innerHTML = "CONEXIÓN EXITOSA ENTRE HTML Y JS";
+
+// Segundo paso
+
+// const nombre = prompt("Podrias indicarme tu nombre?");
+
+// console.log("Tu nombre es: " + nombre);
+// alert("Bienvenido a mi pagina " + nombre + "!");
+
+// Tercer paso
+
+// const entrada = prompt("¿Qué producto desea comprar?");
+
+// console.log(entrada);
+// alert(entrada);
+
+// Cuarto paso
+// nombre = prompt("¿Podrias indicarme tu nombre?");
+
+// const mensaje = "Bienvenido " + nombre;
+// alert (mensaje)
+
+// Actividad 2
+
