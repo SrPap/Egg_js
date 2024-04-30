852f116c7c65357e77b7c6fd44fe8df8fda9bb98
Eros Rodriguez <163212935+SrPap@users.noreply.github.com>
2024-04-29 20:58:20 -0500

    Pack4 de clases

---
+++ egg_js/egg_js9/store2/store_js/index.js
@@ -0,0 +1,64 @@
+// Actividad 1
+
+   // let totalApagar = 0;
+   // let lista = [];
+   // const cantidadProducto = Number(prompt("Cuantos procutos vas a comprar?"));
+
+   // for (let index = 1; index <= cantidadProducto; index++) {
+   // const nombreProducto = prompt("¿Qué producto desea llevar?");
+   // const cantidad = Number(prompt("¿Cuántas unidades?"));
+   // const precio = Number(prompt("¿Cuánto sale cada unidad?" ));
+   // const subtotal = cantidad * precio;k
+   // totalApagar = totalApagar + subtotal;
+   // lista = nombreProducto + " " + lista;
+   // alert( "Compraste " + lista + " y tienes que pagar " + totalApagar)
+   // console.log(`Producto ${i}: ${nombreProducto} - Precio: ${precioProducto} - Total: ${subtotalProducto}`);
+   // console.log(`Precio final de todos tus productos: ${totalApagar}`);
+   // }
+    // document.write('El resultado es: ' + lista);
+
+// Actividad 2
+
+    // const comprar = () => {
+        // let totalApagar = 0;
+        // let lista = ""; // La lista de productos, se puede utilizar [] o "".
+        
+        // for (let index = 1; index <= 3; index++) {
+        // const nombreProducto = prompt("¿Qué producto desea llevar?");
+        // const cantidad = Number(prompt("¿Cuántas unidades?"));
+        // const precio = Number(prompt("¿Cuánto sale cada unidad?"));
+        // const subtotal = cantidad * precio;
+        // totalApagar += subtotal;
+        
+        // Agregar el producto a la lista
+        // if (lista !== "") {
+        // lista += ", "; // Agregar coma si la lista ya tiene elementos
+        // }
+        // lista += `${nombreProducto} (${cantidad} unidades)`;
+        
+        // Mostrar mensaje de compra en consola
+        // console.log(`Compraste ${nombreProducto} (${cantidad} unidades). Total: ${subtotal}`);
+        
+        // No actualizar el DOM dentro del bucle
+        
+        // }
+        
+        // Mostrar mensaje final en el DOM después de completar todas las compras
+        // const mensajeFinal = `Productos comprados: ${lista}. Total a pagar: ${totalApagar}`;
+        // console.log(mensajeFinal);
+        // document.getElementById('miLista').innerHTML = mensajeFinal;
+        
+        // return totalApagar;
+        // };
+        
+        // Llamar a la función comprar() tres veces y sumar los totales
+        // const total1 = comprar();
+        // const total2 = comprar();
+        // const total3 = comprar();
+        // const total = total1 + total2 + total3;
+        
+        // console.log("Tienes que pagar en total " + total);
+
+// Actividad 3
+
+
